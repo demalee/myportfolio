c9f7f3bca162b7234115e975d77196634a6fa4fa
Demalee <dev.demalee@gmail.com>
2026-01-13 09:22:25 +0300

    my-portfolio

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,63 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="Portfolio">
+	<meta name="keywords"
+		content="Creative, minimal, Portfolio, cv, business, Resume, Information, personal portfolio, Professional">
+	<meta name="author" content="kamleshyadav">
+	<meta name="MobileOptimized" content="320">
+	<!--Start Style -->
+	<link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/all.min.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/font.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/animate.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/magnific-popup.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/swiper.min.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/jquery-jvectormap-2.0.3.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/scrollbar.css')}}">
+	<!-- Favicon Link -->
+	<link rel="shortcut icon" type="image/png" href="{{asset('assets/images/favicon.png')}}">
+
+	<title>Wilson Kibet</title>
+</head>
+
+<body><a href="https://kamleshyadav.com/cdn-cgi/content?id=922SBjI75ngQgLBkIjYxWIEL1Hntf6mphHN_XsL9U7c-1768201789.9771314-1.0.1.1-rHtgS.Go3G12q8Vh8m9ys8Pqcp.AX7pul7mvxRK_RDE" aria-hidden="true" rel="nofollow noopener" style="display: none !important; visibility: hidden !important"></a>
+	<!-- preloader section start -->
+	
+
+
+  @include('layouts.header')
+@yield('content')
+
+    <!-- go to top button -->
+	<div class="bottom_top">
+		<i class="fas fa-arrow-up">
+		</i><i class="fas fa-arrow-up"></i>
+	</div>
+
+
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script data-cfasync="false" src="{{asset('cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js')}}"></script>
+    <script src="{{asset('assets/js/jquery.min.js')}}" type="0eb0c5c6eba350b9a6697dd1-text/javascript"></script>
+	<script src="{{asset('assets/js/bootstrap.min.js')}}" type="0eb0c5c6eba350b9a6697dd1-text/javascript"></script>
+	<script src="{{asset('assets/js/cvtext1.js')}}" type="0eb0c5c6eba350b9a6697dd1-text/javascript"></script>
+	<script src="{{asset('assets/js/cvtext2.js')}}" type="0eb0c5c6eba350b9a6697dd1-text/javascript"></script>
+	<script src="{{asset('assets/js/isotope.pkgd.min.js')}}" type="0eb0c5c6eba350b9a6697dd1-text/javascript"></script>
+	<script src="{{asset('assets/js/jquery.magnific-popup.min.js')}}" type="0eb0c5c6eba350b9a6697dd1-text/javascript"></script>
+	<script src="{{asset('assets/js/swiper.min.js')}}" type="0eb0c5c6eba350b9a6697dd1-text/javascript"></script>
+	<script src="{{asset('assets/js/circle-progress.js')}}" type="0eb0c5c6eba350b9a6697dd1-text/javascript"></script>
+	<!-- JQUERY-JVECTORMAP JS -->
+	<script src="{{asset('assets/js/jquery-jvectormap.min.js')}}" type="0eb0c5c6eba350b9a6697dd1-text/javascript"></script>
+	<!-- JVECTORMAP-WORLD-MILL.JS JS -->
+	<script src="{{asset('assets/js/jquery-jvectormap-world-mill.js')}}" type="0eb0c5c6eba350b9a6697dd1-text/javascript"></script>
+	<script src="{{asset('assets/js/scrollbar.js')}}" type="0eb0c5c6eba350b9a6697dd1-text/javascript"></script>
+	<script src="{{asset('assets/js/custom.js')}}" type="0eb0c5c6eba350b9a6697dd1-text/javascript"></script>
+<script src="{{asset('cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js')}}" data-cf-settings="0eb0c5c6eba350b9a6697dd1-|49" defer></script><script>(function(){function c(){var b=a.contentDocument||a.contentWindow.document;if(b){var d=b.createElement('script');d.innerHTML="window.__CF$cv$params={r:'9bcad4a35b461e43',t:'MTc2ODIwMTc4OQ=='};var a=document.createElement('script');a.src='/cdn-cgi/challenge-platform/scripts/jsd/main.js';document.getElementsByTagName('head')[0].appendChild(a);";b.getElementsByTagName('head')[0].appendChild(d)}}if(document.body){var a=document.createElement('iframe');a.height=1;a.width=1;a.style.position='absolute';a.style.top=0;a.style.left=0;a.style.border='none';a.style.visibility='hidden';document.body.appendChild(a);if('loading'!==document.readyState)c();else if(window.addEventListener)document.addEventListener('DOMContentLoaded',c);else{var e=document.onreadystatechange||function(){};document.onreadystatechange=function(b){e(b);'loading'!==document.readyState&&(document.onreadystatechange=e,c())}}}})();</script></body>
+
+</html>
